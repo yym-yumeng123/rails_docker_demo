35f5eb25382de2b3edf78f4f1c257bcd69588e3b
yym-yumeng123 <18026493873@163.com>
2021-09-23 16:43:44 +0800

    rspec test user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,25 @@ RSpec.describe User, type: :model do
     user = User.create email: '1@qq.com', password: '123456', password_confirmation: '123456'
     expect { User.destroy_by id: user.id }.to change { User.count }.from(1).to(0)
   end
+
+  # 测试 user.rb 里面的内容
+  it 'password must presence' do
+    user = User.create password: '123456', password_confirmation: '123456'
+    expect(user.errors.details[:email][0][:error]).to eq(:blank)
+  end
+
+  it 'email nust uniqueness' do
+    User.create! email: '1@qq.com', password: '123456', password_confirmation: '123456'
+    user = User.create email: '1@qq.com', password: '123456', password_confirmation: '123456'
+    expect(user.errors.details[:email][0][:error]).to eq(:taken)
+  end
+
+  xit 'email must send mailer' do
+    mailer = spy ('mailer')
+    allow(UserMailer).to receive(:welcome_email).and_return(mailer)
+    User.create! email: '1@qq.com', password: '123456', password_confirmation: '123456'
+    expect(UserMailer).to have_received(:welcome_email)
+    expect(mailer).to have_received(:deliver_later)
+    
+  end
 end
