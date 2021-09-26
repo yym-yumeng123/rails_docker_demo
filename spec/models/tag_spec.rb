8361a6da024a9531fb4b3e2261da3af09581d013
yym-yumeng123 <18026493873@163.com>
2021-09-26 17:33:59 +0800

    test tag 必传

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'create' do
+    it 'name must presence' do
+      tag = Tag.create 
+      p tag.errors
+
+      expect(tag.errors.details[:name][0][:error]).to eq(:blank)
+      expect(tag.errors[:name][0]).to eq('标签名不能为空')
+    end
+  end
 end
