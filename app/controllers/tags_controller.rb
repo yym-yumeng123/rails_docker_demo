74d3fa244194fc7cc16e510f465f73ee2790bece
yym-yumeng123 <18026493873@163.com>
2021-09-27 16:02:08 +0800

    index & show & update

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -10,6 +10,20 @@ class TagsController < ApplicationController
     head tag.destroy ? :ok : :bad_request
   end
 
+  def index
+    render_index_resources  Tag.page(params[:page]).per(params[:per_page])
+  end
+
+  def show
+    render_resources Tag.find(params[:id])
+  end
+
+  def update
+    tag = Tag.find(params[:id])
+    tag.update(create_params)
+    render_resources tag
+  end
+
   private
   def create_params
     params.permit(:name)
--- spec/requests/tags_request_spec.rb
+++ spec/requests/tags_request_spec.rb
@@ -40,4 +40,58 @@ RSpec.describe "Tags", type: :request do
       expect(response.status).to be 200
     end
   end
+
+  context 'index' do
+    it 'should not get tags before sign in' do
+      get '/tags'
+      expect(response.status).to be 401
+    end
+
+    it 'should get tags after sign in' do
+      (1..11).to_a.map do |n|
+        Tag.create! name: "#{n}"
+      end
+      sign_in
+      get '/tags'
+      expect(response.status).to be 200
+      body = JSON.parse response.body
+      expect(body['resources'].length).to eq 10
+    end
+  end
+
+  context 'show' do
+    it 'should not get a tag before sign in' do
+      tag = Tag.create! name: 'xxx'
+      get "/tags/#{tag.id}"
+      expect(response.status).to be 401
+    end
+    it 'should get a tag after sign in' do
+      sign_in
+      tag = Tag.create! name: 'xxx'
+      get "/tags/#{tag.id}"
+      expect(response.status).to be 200
+    end
+    it 'should not get a tag because not fount' do
+      sign_in
+      get "/tags/999999999999999999"
+      expect(response.status).to be 404
+    end
+  end
+
+  context 'update' do
+    it 'should not update a tag before sign in' do
+      tag = Tag.create! name: 'xxxxx'
+      patch "/tags/#{tag.id}", params: {name: 'yyyy'}
+      expect(response.status).to eq 401
+    end
+
+    it 'should update a tag' do
+      sign_in
+      tag = Tag.create! name: 'xxxxx'
+      patch "/tags/#{tag.id}", params: {name: 'yyyy'}
+      expect(response.status).to eq 200
+      body = JSON.parse response.body
+      expect(body["resources"]["name"]).to eq 'yyyy'
+    end
+  end
 end
