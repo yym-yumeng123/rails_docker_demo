1cad656a44c508ea37c7a69d6f630d92c5a3247b
yym-yumeng123 <18026493873@163.com>
2021-09-28 16:31:47 +0800

    测试 n : n 的 tagging

--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -14,4 +14,27 @@ RSpec.describe Tagging, type: :model do
     expect(tagging.errors.details[:tag][0][:error]).to eq(:blank)
     expect(tagging.errors[:tag][0]).to eq('标签不能为空')
   end
+
+  it 'has record and tag' do
+    tag = Tag.create! name: 'test'
+    record = Record.create! amount: 10000, category: 'income'
+    tagging = Tagging.create record: record, tag: tag
+    expect(tag.records.first.id).to eq record.id
+    expect(record.tags.first.id).to eq tag.id
+  end
+
+  it 'has records and tags' do
+    tag1 = Tag.create! name: 'test1'
+    tag2 = Tag.create! name: 'test2'
+    record1 = Record.create! amount: 10000, category: 'income'
+    record2 = Record.create! amount: 10000, category: 'income'
+    Tagging.create record: record1, tag: tag1
+    Tagging.create record: record1, tag: tag2
+    Tagging.create record: record2, tag: tag1
+    Tagging.create record: record2, tag: tag2
+    expect(tag1.records.count).to eq 2
+    expect(tag2.records.count).to eq 2
+    expect(record1.tags.count).to eq 2
+    expect(record2.tags.count).to eq 2
+  end
 end
